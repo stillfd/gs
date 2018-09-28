757048f718e059d2950f11eb15f6cee8600964be
stillfd <stillfd@163.com>
2018-09-28 21:00:26 +0800

    update food function and death detection

--- greedy_snake/gs/gs/head.h
+++ greedy_snake/gs/gs/head.h
@@ -26,3 +26,6 @@ char g_strGameBack[20][48] = {  "¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö¡ö\
 enum {to_east = 2, to_west = -2, to_north = -1, to_south = 1};
 // array of snake
 int g_arrSnake[DEF_SNAKE_LONG][3] = { 0 };
+
+//DIR OF SNAKE
+int g_nSnakeDir = to_west;
--- greedy_snake/gs/gs/snake.c
+++ greedy_snake/gs/gs/snake.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<windef.h>
 #include<conio.h>
 #include<time.h>
 
@@ -30,7 +30,17 @@ void DrawSnake(void);
 // snake go
 void SnakeMove(void);
 
+//destroy snake
+void DestroySnake(void);
 
+// changd dir
+void ChangeDir(void);
+
+//snake die
+char IsSnakeDie(void);
+
+//get food
+void GenFood(void);
 int main(void)
 {
 
@@ -51,10 +61,21 @@ int main(void)
 
 	SetSnakeRandPos();
 	
+	GenFood();
+
 	while (1)
 	{
 		system("cls");
+
+		ChangeDir();
 		
+		if (1 == IsSnakeDie())
+		{
+			printf("\t\tSNAKE DIED!\n");
+			break;
+		}
+		
+
 		SnakeMove();
 
 		showBackGround();
@@ -143,6 +164,7 @@ void DrawSnake(void)
 
 void SnakeMove(void) 
 {
+	DestroySnake();
 	int i = DEF_SNAKE_LONG;
 	for (i; i >= 1; i--)
 	{
@@ -153,6 +175,9 @@ void SnakeMove(void)
 		g_arrSnake[i][1] = g_arrSnake[i - 1][1];
 		g_arrSnake[i][2] = g_arrSnake[i - 1][2];
 	}
+
+	
+
 	if (to_west == g_arrSnake[0][2] || to_east == g_arrSnake[0][2])
 	{ 
 		g_arrSnake[0][1] += g_arrSnake[0][2];
@@ -162,4 +187,84 @@ void SnakeMove(void)
 		g_arrSnake[0][0] += g_arrSnake[0][2];
 	}
 	DrawSnake();
+}
+
+void DestroySnake(void) 
+{
+	int i = 0;
+	for (i = 0; g_arrSnake[i][0] != 0; i++)
+	{
+		// use strncpy to avoid "\0"
+		strncpy(&g_strGameBack[g_arrSnake[i][0]][g_arrSnake[i][1]], "  ", 2);
+	}
+}
+
+void ChangeDir(void) 
+{
+	if(GetAsyncKeyState('W'))
+	{
+		g_nSnakeDir = to_north;
+	}
+	else if (GetAsyncKeyState('S'))
+	{
+		g_nSnakeDir = to_south;
+	}
+	else if (GetAsyncKeyState('A'))
+	{
+		g_nSnakeDir = to_west;
+	}
+	else if (GetAsyncKeyState('D'))
+	{
+		g_nSnakeDir = to_east;
+	}
+	
+	g_arrSnake[0][2] = g_nSnakeDir;
+}
+
+char IsSnakeDie(void) 
+{
+	if (to_west == g_arrSnake[0][2] || to_east == g_arrSnake[0][2])
+	{
+		if(0 == strncmp(&g_strGameBack[g_arrSnake[0][0]][g_arrSnake[0][1] + g_arrSnake[0][2]],"¡ö",2))
+		{
+			return 1;
+		}
+	}
+	else
+	{
+		if (0 == strncmp(&g_strGameBack[g_arrSnake[0][0]+ g_arrSnake[0][2]][g_arrSnake[0][1]], "¡ö", 2))
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+void GenFood(void)
+{
+	srand((unsigned int)time(NULL));
+	int nRow, nCol;
+	while (1)
+	{
+		nRow = rand() % 18 + 1;
+		nCol = rand() % 21 + 1;
+
+		char flag = 1;
+		// detect whether the food and the snake are overlapped
+		for (int i = 0; g_arrSnake[i][0] != 0; i++)
+		{
+			if (nRow == g_arrSnake[i][0] && nCol*2 == g_arrSnake[i][1])
+			{
+				flag = 0;
+				break;
+			}
+		}
+		if (flag)
+		{ 
+			break;
+		}
+	}
+	//plot food
+	strncpy(&g_strGameBack[nRow][nCol * 2], "¡ø", 2);
 }
